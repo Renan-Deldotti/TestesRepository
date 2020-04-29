41bef87ad62b9b51592dc52995d9ad47d378dd45
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-04-29 00:28:11 -0300

    Update

--- ArchitectureExample/app/src/main/java/com/example/architectureexample/MainActivity.java
+++ ArchitectureExample/app/src/main/java/com/example/architectureexample/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.architectureexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NoteViewModel noteViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //noteViewModel = new ViewModelProvider(this);
+
     }
 }
