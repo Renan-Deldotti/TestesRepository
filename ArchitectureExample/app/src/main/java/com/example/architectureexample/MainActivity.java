8e129ec8c5241e3d607e6f0ac181dfe17bb53982
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-04-29 01:43:45 -0300

    Update MainActivity.java

--- ArchitectureExample/app/src/main/java/com/example/architectureexample/MainActivity.java
+++ ArchitectureExample/app/src/main/java/com/example/architectureexample/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.architectureexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //noteViewModel = new ViewModelProvider(this);
+//        Youtube answers
+        noteViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication()).create(NoteViewModel.class);
+//        noteViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(NoteViewModel.class);
+//
+//        Stackoverflow answer:
+        //noteViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication()).create(NoteViewModel.class);
 
+        // Google Codelab answer:
+        //noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // Todo: iniciate recyclerView
+                Toast.makeText(MainActivity.this, "Changed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
