18f519465872b7ad4fd342ef14e98500ef4d1675
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-04-29 17:20:32 -0300

    Update 6

--- ArchitectureExample/app/src/main/java/com/example/architectureexample/MainActivity.java
+++ ArchitectureExample/app/src/main/java/com/example/architectureexample/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.architectureexample;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -18,14 +20,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         // Youtube answer: Working
         noteViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication()).create(NoteViewModel.class);
 
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                // Todo: iniciate recyclerView
-                Toast.makeText(MainActivity.this, "Changed", Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
             }
         });
     }
