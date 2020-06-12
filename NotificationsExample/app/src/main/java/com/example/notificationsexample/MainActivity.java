d2c97024d74b8216277e0061d00f24f1b857db91
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-06-11 22:25:00 -0300

    UpdateN2

--- NotificationsExample/app/src/main/java/com/example/notificationsexample/MainActivity.java
+++ NotificationsExample/app/src/main/java/com/example/notificationsexample/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -29,11 +32,25 @@ public class MainActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
 
-        Notification notification = new NotificationCompat.Builder(this,App.CHANNEL_1_ID)
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
+
+        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
+        broadcastIntent.putExtra("toastMessage", message);
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_adb)
                 .setContentTitle(title)
+                .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.BLUE)
+                .setContentIntent(contentIntent) // Intent to open this activity
+                .setAutoCancel(true) // Auto dismiss when clicked
+                .addAction(R.drawable.ic_adb, "Toast", actionIntent) // Button (max3)
+                .setOnlyAlertOnce(true) // Ony alert once then don't make any sound
                 .build();
 
         notificationManagerCompat.notify(1, notification);
@@ -43,9 +60,10 @@ public class MainActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
 
-        Notification notification = new NotificationCompat.Builder(this,App.CHANNEL_2_ID)
+        Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_adb)
                 .setContentTitle(title)
+                .setContentText(message)
                 .setPriority(NotificationCompat.PRIORITY_LOW)
                 .build();
 
