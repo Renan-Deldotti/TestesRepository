d40cf7de00f9d0eed5a59e97073d4d0f8c48aa29
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-06-13 00:29:28 -0300

    Update

--- NotificationsExample/app/src/main/java/com/example/notificationsexample/MainActivity.java
+++ NotificationsExample/app/src/main/java/com/example/notificationsexample/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.core.app.NotificationManagerCompat;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -35,21 +37,26 @@ public class MainActivity extends AppCompatActivity {
         Intent activityIntent = new Intent(this, MainActivity.class);
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, activityIntent, 0);
 
-        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
+        /*Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
         broadcastIntent.putExtra("toastMessage", message);
         PendingIntent actionIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        */
 
+        Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.dog_placeholder);
 
         Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_adb)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setLargeIcon(picture)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(picture)
+                        .bigLargeIcon(null))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .setColor(Color.BLUE)
                 .setContentIntent(contentIntent) // Intent to open this activity
                 .setAutoCancel(true) // Auto dismiss when clicked
-                .addAction(R.drawable.ic_adb, "Toast", actionIntent) // Button (max3)
+                //.addAction(R.drawable.ic_adb, "Toast", actionIntent) // Button (max3)
                 .setOnlyAlertOnce(true) // Ony alert once then don't make any sound
                 .build();
 
@@ -60,11 +67,24 @@ public class MainActivity extends AppCompatActivity {
         String title = editTextTitle.getText().toString();
         String message = editTextMessage.getText().toString();
 
+        Bitmap artwork = BitmapFactory.decodeResource(getResources(), R.drawable.dog_placeholder);
+
+        // If using channel use CATEGORY_TRANSPORT
+
         Notification notification = new NotificationCompat.Builder(this, App.CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_adb)
                 .setContentTitle(title)
                 .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setLargeIcon(artwork)
+                .addAction(R.drawable.ic_thumb_down, "Dislike", null) // Should pass a pending intent
+                .addAction(R.drawable.ic_baseline_arrow_back_ios_24, "Previous", null)
+                .addAction(R.drawable.ic_baseline_play_arrow_24, "Play", null)
+                .addAction(R.drawable.ic_baseline_navigate_next_24, "Pause", null)
+                .addAction(R.drawable.ic_thumb_up, "Like", null)
+                .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
+                        .setShowActionsInCompactView(1, 2, 3))
+                .setSubText("Sub text")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .build();
 
         notificationManagerCompat.notify(2, notification);
